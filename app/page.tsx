369d96506d9be448099ef46725874be1ebdfb576
Vicente Venegas <vicente.venegas997@gmail.com>
2025-09-01 12:30:19 -0400

    Tagline

--- app/page.tsx
+++ app/page.tsx
@@ -3,14 +3,15 @@ import Typewriter from "@/components/Typewriter";
 export default function Page() {
   return (
     <>
-      <section className="container grid-6 gap-y-6 pt-16 pb-8">
-        <h1 className="col-span-6 lg:col-span-5 max-w-[28ch] text-[48px] leading-[1.1] font-semibold text-white">
-          <Typewriter text="UX/UI designer creating products that unite research, interfaces, and technology." speed={20} />
+      <section className="container grid-6 gap-y-0 pt-16 pb-8">
+        <h1 className="col-span-6 lg:col-span-3 lg:col-start-1 max-w-[28ch] text-[48px] leading-[1.1] font-bold text-white">
+          <Typewriter text="I design user experiences from research to interface." speed={20} />
         </h1>
-        <p className="col-span-6 lg:col-span-4 mt-6 max-w-[46ch] text-[32px] leading-[1.35] text-white/70">
-          <Typewriter text="Exploring the intersection of interfaces, code, and tangible design." speed={18} startDelay={1400} />
+        <p className="col-span-6 lg:col-span-2 lg:col-start-1 lg:row-start-2 mt-6 max-w-[46ch] text-[32px] leading-[1.35] text-white/70">
+          <Typewriter text="Blending interaction, code, and tangible design." speed={18} startDelay={1400} />
         </p>
       </section>
+
 
       <section id="projects" className="container grid-6 gap-y-12 pb-24">
         {/* ...cards go here using col-span-* */}
